c6a49f630bde2332e43bc7ae46372f3a9a4a8c4f
amolsr <amol.saini567@gmail.com>
2021-02-04 00:02:43 +0530

    banner added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,51 +1,38 @@
 import React from "react";
 import Header from "./_include/_header";
 import Footer from "./_include/_footer";
-import { Box, Button, Container } from "@material-ui/core";
+import { Box } from "@material-ui/core";
 import Carousel from "react-material-ui-carousel";
-import { Paper } from "@material-ui/core";
 
 export default function Main() {
   var items = [
-    {
-      name: "Random Name #1",
-      description: "Probably the most random thing you have ever seen!",
-    },
-    {
-      name: "Random Name #2",
-      description: "Hello World!",
-    },
+    process.env.PUBLIC_URL + "/image/1.jpg",
+    process.env.PUBLIC_URL + "/image/2.jpg",
+    process.env.PUBLIC_URL + "/image/33.jpg",
+    process.env.PUBLIC_URL + "/image/ph.jpg",
+    process.env.PUBLIC_URL + "/image/pho.jpg",
   ];
 
   return (
     <div>
       <Header />
-
-      <Carousel>
-        {items.map((item, i) => (
-          <Item key={i} item={item} />
-        ))}
-      </Carousel>
-      <Box my={2}>
-        {[...new Array(122)]
-          .map(
-            () =>
-              `Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. `
-          )
-          .join("\n")}
+      <Box style={{ zIndex: -1, marginTop: "-10vh" }}>
+        <Carousel animation="fade">
+          {items.map((item, i) => (
+            <div
+              style={{
+                backgroundImage: `url(${item})`,
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                height: "100vh",
+                width: "100%",
+              }}
+            ></div>
+          ))}
+        </Carousel>
       </Box>
       <Footer />
     </div>
-  );
-}
-
-function Item(props) {
-  return (
-    <Paper>
-      <h2>{props.item.name}</h2>
-      <p>{props.item.description}</p>
-
-      <Button className="CheckButton">Check it out!</Button>
-    </Paper>
   );
 }
