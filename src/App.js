f21d657af4792f46b971a0702740ce882b3f6b2c
amolsr <amol.saini567@gmail.com>
2021-02-05 15:23:37 +0530

    contact us added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Main from "./components/Main";
 import "./App.css";
 
-function App() {
-  return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <Switch>
-        <Route path="/" component={Main} />
-        <Route path="*">
-          <h1>Not Found</h1>
-        </Route>
-      </Switch>
-    </Router>
-  );
+class App extends React.Component {
+  state = {
+    loading: true,
+  };
+
+  componentDidMount() {
+    // this simulates an async action, after which the component will render the content
+    demoAsyncCall().then(() => this.setState({ loading: false }));
+  }
+
+  render() {
+    const { loading } = this.state;
+
+    if (loading) {
+      // if your component doesn't have to wait for an async action, remove this block
+      return null; // render null when app is not ready
+    }
+
+    return (
+      <Router basename={process.env.PUBLIC_URL}>
+        <Switch>
+          <Route path="/" component={Main} />
+          <Route path="*">
+            <h1>Not Found</h1>
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+function demoAsyncCall() {
+  return new Promise((resolve) => setTimeout(() => resolve(), 4000));
 }
 
 export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -7,11 +7,32 @@ import {
   Grid,
   makeStyles,
   Typography,
+  Paper,
+  TextField,
+  Button,
 } from "@material-ui/core";
 import Carousel from "react-material-ui-carousel";
+import PhoneIcon from "@material-ui/icons/Phone";
+import EmailIcon from "@material-ui/icons/Email";
 
 const useStyle = makeStyles((theme) => ({
   img: { maxWidth: "100%", maxHeight: "100%" },
+  root: {
+    margin: theme.spacing(10, 0),
+  },
+  icon: {
+    display: "inline-block",
+    width: "40px",
+    height: "40px",
+    fontSize: "18px",
+    borderColor: "#ff7902",
+    color: "#ff7902",
+    borderRadius: "50%",
+    textAlign: "center",
+    lineHeight: "36px",
+    marginRight: "10px",
+    marginBottom: "10px",
+  },
 }));
 
 export default function Main() {
@@ -91,7 +112,7 @@ export default function Main() {
                 backgroundPosition: "center",
                 backgroundRepeat: "no-repeat",
                 backgroundSize: "cover",
-                height: "100vh",
+                height: "102vh",
                 width: "100%",
               }}
             ></div>
@@ -280,6 +301,110 @@ export default function Main() {
             </Grid>
           </Grid>
         </Container>
+      </Box>
+      <Box>
+        <Grid
+          container
+          justify="center"
+          alignItems="center"
+          spacing={6}
+          style={{
+            margin: "5vh 0vh",
+            width: "100%",
+          }}
+        >
+          <Grid item md={12} lg={6}>
+            <Typography variant="h3" gutterBottom>
+              CONTACT US <span style={{ color: "#ff7902" }}> TODAY NOW</span>
+            </Typography>
+            <Grid container>
+              <Grid item xs={12} md={6}>
+                <Typography variant="body1" gutterBottom>
+                  <p style={{ display: "flex" }}>
+                    <PhoneIcon className={classes.icon} />
+                    +91-9958683725
+                    <br />
+                    +91-9818434636
+                  </p>
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Typography variant="body1" gutterBottom>
+                  <p style={{ display: "flex" }}>
+                    <EmailIcon className={classes.icon} />
+                    info@ramafilmcompany.com <br />
+                    enquiry@ramafilmcompany.com
+                  </p>
+                </Typography>
+              </Grid>
+              {/* <Grid container spacing={4}>
+                <Grid item xs={12} md={6}>
+                  <img
+                    src={process.env.PUBLIC_URL + "/image/13.jpg"}
+                    alt="sect1"
+                    className={classes.card}
+                  />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <img
+                    src={process.env.PUBLIC_URL + "/image/14.jpg"}
+                    alt="sect1"
+                    className={classes.card}
+                  />
+                </Grid>
+              </Grid> */}
+            </Grid>
+          </Grid>
+
+          <Grid item component={Paper} md={12} lg={6}>
+            {" "}
+            <Typography variant="h4" gutterBottom>
+              WHAT WOULD YOU LIKE TO TALK ABOUT
+            </Typography>
+            <Typography variant="h5" style={{ color: "#ff7902" }}>
+              We'd Love to Hear From you !
+            </Typography>
+            <form className={classes.root} noValidate autoComplete="off">
+              <Grid container justify="center" alignItems="center" spacing={2}>
+                <Grid item xs={12} md={6} compoent={TextField}>
+                  <TextField
+                    id="outlined-basic"
+                    label="Name"
+                    variant="outlined"
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    id="outlined-basic"
+                    label="Email"
+                    variant="outlined"
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  {" "}
+                  <TextField
+                    id="outlined-basic"
+                    label="Your Message"
+                    variant="outlined"
+                    multiline
+                    fullWidth
+                    rows={4}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Button
+                    variant="contained"
+                    style={{ backgroundColor: "#ff7902", color: "white" }}
+                  >
+                    Submit Now
+                  </Button>
+                </Grid>
+              </Grid>
+            </form>
+          </Grid>
+        </Grid>
       </Box>
       <Footer />
     </div>
